24924399ac9f87c1047bd9e2ba6a22aa1578b59c
Rakin Rahman <rakinhacker@gmail.com>
2022-07-30 03:14:21 +0600

    it works now

--- mouse_pass.py
+++ mouse_pass.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 from pynput.mouse import Listener, Button
 
+PIN = "1101"
+PIN_LENGTH = len(PIN)
+user_pin = ""
+
 
 def on_click(x, y, button, pressed):
+    global user_pin
     # Detect left and right mouse clicks
     if pressed and button == Button.left:
-        print("Left click")
+        user_pin = user_pin + "1"
+        print(user_pin)     # debugging
     elif pressed and button == Button.right:
-        print("Right click")
+        user_pin = user_pin + "0"
+        print(user_pin)     # debugging
+
+    # Stop monitoring mouse clicks when max pin length is reached
+    if len(user_pin) == PIN_LENGTH:
+        # Show whether password is right or not
+        if user_pin == PIN:
+            print("Correct password")
+        else:
+            print("Wrong password")
+        return False
 
 
 with Listener(on_click=on_click) as listener:

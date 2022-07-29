f3cb19f2e35f4e85085d18e00bf1c11dd26d54aa
Rakin Rahman <rakinhacker@gmail.com>
2022-07-30 02:20:16 +0600

    Detect mouse clicks

--- mouse_pass.py
+++ mouse_pass.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+from pynput.mouse import Listener, Button
 
-print("Hello World")
+
+def on_click(x, y, button, pressed):
+    # Detect left and right mouse clicks
+    if pressed and button == Button.left:
+        print("Left click")
+    elif pressed and button == Button.right:
+        print("Right click")
+
+
+with Listener(on_click=on_click) as listener:
+    listener.join()
